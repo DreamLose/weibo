23997f117a0597edc593404fffb6facd9b49cbbb
DreamLose <1083531814@qq.com>
2020-07-24 17:53:48 +0800

    初始化项目

---
+++ weibo/weibo/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  weibo
+//
+//  Created by 2020 on 2020/7/24.
+//  Copyright © 2020 2020. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UITabBar.appearance().tintColor = UIColor.orange
+        return true
+    }
+
+
+}
+
